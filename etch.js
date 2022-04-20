2d9ec2584d03d2dd4aba10a78278963d987a3086
Ian <ian.forest22@gmail.com>
2022-04-20 14:12:39 -0700

    add ability to change cell color on mousover

--- etch.js
+++ etch.js
@@ -8,6 +8,11 @@ pixel.style = "width: 100% ; height: 100% ; background-color: blue"
 container.appendChild(pixel)
 }
 
-container.style = `grid-template-columns: repeat(${16}, 1fr) ; gap: 5px`;
+let pixels = document.querySelectorAll('.container div')
 
+container.style = `grid-template-columns: repeat(${16}, 1fr) ; gap: 2px`;
+
+
+for (const pixel of pixels) {
 pixel.addEventListener('mouseover', function() {pixel.style = "background-color: red"})
+}
